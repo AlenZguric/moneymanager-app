730e9b3390c94c995c576ee51745e37464f70f72
Alen Zgurić <zguric.alen@hotmail.com>
2023-06-06 01:02:10 +0200

    remowe firebaseconfig with gitignore

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_-vtgQXjxHf9QKamqVkuukRqOjlQSGk8",
+  authDomain: "money-manager-54896.firebaseapp.com",
+  projectId: "money-manager-54896",
+  storageBucket: "money-manager-54896.appspot.com",
+  messagingSenderId: "636795609634",
+  appId: "1:636795609634:web:270e32d7ee1491353a607b",
+  measurementId: "G-TPJYWVS7PK"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
