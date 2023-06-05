1e49871f067a8500f1e4d469f6bd63fd379913aa
Alen Zgurić <zguric.alen@hotmail.com>
2023-06-06 01:31:32 +0200

    login success

--- src/components/Auth/Signin.jsx
+++ src/components/Auth/Signin.jsx
@@ -1,40 +1,37 @@
-import React, { useState } from "react";
-import { signIn } from "../../firebase/firebaseUtils";
+import React, { useState } from 'react';
+import firebase from '../../firebase/firebaseUtils';
 
-const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSignIn = async (e) => {
-    e.preventDefault();
+  const handleSignIn = async () => {
     try {
-      await signIn(email, password);
-      // Uspješna prijava
+      // Koristi firebase objekt za prijavu korisnika
+      await firebase.signInWithEmailAndPassword(email, password);
+      console.log("Firebase");
+      // Uspješna prijava - dodaj potrebne logiku
     } catch (error) {
-      // Greška pri prijavi
+      // Greška pri prijavi - obradi grešku
     }
   };
 
   return (
     <div>
       <h2>Prijava</h2>
-      <form onSubmit={handleSignIn}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Lozinka"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Prijavi se</button>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Lozinka:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="button" onClick={handleSignIn}>Prijavi se</button>
       </form>
     </div>
   );
 };
 
-export default SignIn;
+export default Signin;
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD_-vtgQXjxHf9QKamqVkuukRqOjlQSGk8",
   authDomain: "money-manager-54896.firebaseapp.com",
   projectId: "money-manager-54896",
